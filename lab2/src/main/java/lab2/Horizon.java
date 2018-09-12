96d3168a9c9f38faf5be78a07087839560fb64f0
Vidaaaasb <natyregaming@gmail.com>
2018-09-12 18:07:31 +0300

    Update lab2

--- lab2/src/main/java/lab2/Horizon.java
+++ lab2/src/main/java/lab2/Horizon.java
@@ -8,14 +8,12 @@ public class Horizon
         String group = "ИВТ/б-11-о";
         String str = str(lastName, group);
 
-        int a = 25; //шестнадцатиричка
-        short b = 67;//восмиричка
-        byte c = -3;//Дисетиричка, наличие ведущих нулей, ширина 8(мин кол-во цифр)
-        float f = 23.122f;//Наличие ведущих нулей, Ширина 9, точность 4
-        double d = 124.21412151212124;//Ширина 7, Точность 3( кол-во цифр после запятой, обязательный вывод знака, отсутствие ведущих нулей
-
-        System.out.printf("str=%s, a= %x, b=%o, c=%08d, f=%09.4f, d=%7.3f \n", str, a, b, c, f, d);
-
+        int a = 25;
+        short b = 67;
+        byte c = -3;
+        float f = 23.122f;
+        double d = 124.21412151212124;
+        System.out.printf("Привет, %s, a= %x, b=%o, c=%08d, d=%7.3f! \n", str, a, b, c, f, d);
     }
     public static String str(String a, String b)
     {
